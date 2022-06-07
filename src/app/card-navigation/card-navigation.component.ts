13c434ed027ccd442feb9aa7138f24a0838a8dd2
Stefano SILVESTRINI <stefano.silvestrini@alten.it>
2022-06-07 09:51:41 +0200

    navigation in pokemon card done

--- src/app/card-navigation/card-navigation.component.ts
+++ src/app/card-navigation/card-navigation.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormatService } from '../services/format.service';
 
 @Component({
   selector: 'app-card-navigation',
   templateUrl: './card-navigation.component.html',
-  styleUrls: ['./card-navigation.component.scss']
+  styleUrls: ['./card-navigation.component.scss'],
 })
 export class CardNavigationComponent implements OnInit {
+  currentNumber: number;
+  constructor(
+    private formatService: FormatService,
+    private route: ActivatedRoute
+  ) {}
 
-  constructor() { }
+  formatNumber = this.formatService.getPrettyNumber;
 
   ngOnInit(): void {
+    this.currentNumber = Number(this.route.snapshot.params['pokemonId']);
+    this.route.params.subscribe((changes) => {
+      this.currentNumber = Number(changes['pokemonId']);
+    });
   }
-
 }
