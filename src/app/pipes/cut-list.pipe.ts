6b5a9b66ca4bf85c7a66ea46bd554ea1571bd8e3
StSilvestrini <stefano.silvestrini@alten.it>
2022-06-23 16:15:36 +0200

    console.logs removed

--- src/app/pipes/cut-list.pipe.ts
+++ src/app/pipes/cut-list.pipe.ts
@@ -5,7 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CutListPipe implements PipeTransform {
   transform(value: any[], cuttingPoint: number = 10): any[] {
-    console.log('value', value);
     return value?.length > cuttingPoint ? value.slice(0, cuttingPoint) : value;
   }
 }
