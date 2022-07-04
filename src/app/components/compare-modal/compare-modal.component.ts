89c27f01931204efb1913d4def0b760dc1a8ff0b
StSilvestrini <stefano.silvestrini@alten.it>
2022-07-04 13:06:40 +0200

    code cleaned

--- src/app/components/compare-modal/compare-modal.component.ts
+++ src/app/components/compare-modal/compare-modal.component.ts
@@ -6,7 +6,7 @@ import {
   OnInit,
   Output,
 } from '@angular/core';
-import { mergeMap, Subscription, take } from 'rxjs';
+import { mergeMap, Subscription } from 'rxjs';
 import { ArrayManipulationService } from 'src/app/services/arrayManipulation.service';
 import { HttpPokedexService } from 'src/app/services/http.service';
 import { StoreService } from 'src/app/services/store.service';
@@ -39,56 +39,24 @@ export class CompareModalComponent implements OnInit, OnDestroy {
             this.storeService.dispatchPokemonCard(data.pokemonCard);
           return this.httpService.getPokemonCard(this?.comparePokemons?.[1]);
         })
-        // take(1)
       )
       .subscribe((data) => {
         if (!data?.isInStore)
           this.storeService.dispatchPokemonCard(data.pokemonCard);
         this.comparePokemons = [this.comparePokemons[0], data.pokemonCard];
         this.comparePokemons = this.comparePokemons.map((el, index) => {
-          return { ...el, winningChances: this.getWinningChance()?.[index] };
+          return {
+            ...el,
+            winningChances: this.getWinningChance(this.comparePokemons)?.[
+              index
+            ],
+          };
         });
       });
   }
 
-  getTotal = (damageRelationsArray, baseExperience, typeArray) => {
-    damageRelationsArray.forEach((el) => {
-      const firstKey = Object.keys(el)?.[0];
-      switch (firstKey) {
-        case 'double_damage_from':
-          if (
-            this.utilityService.hasCommonElement(
-              el['double_damage_from'],
-              typeArray
-            )
-          ) {
-            baseExperience = baseExperience * 0.7;
-          }
-        case 'half_damage_from':
-          if (
-            this.utilityService.hasCommonElement(
-              el['half_damage_from'],
-              typeArray
-            )
-          ) {
-            baseExperience = baseExperience * 1.2;
-          }
-        case 'no_damage_from':
-          if (
-            this.utilityService.hasCommonElement(
-              el['no_damage_from'],
-              typeArray
-            )
-          ) {
-            baseExperience = baseExperience * 1.5;
-          }
-      }
-    });
-    return Math.round(baseExperience * 100) / 100;
-  };
-
-  getWinningChance = () => {
-    const pokemonData = this.comparePokemons.map((pokemon) => {
+  getWinningChance = (pokemonsToCompare: any[]) => {
+    const pokemonData = pokemonsToCompare.map((pokemon) => {
       const damage_relations = this.arrayManipulationService.getTypeProps(
         pokemon,
         'damage_relations'
@@ -99,17 +67,17 @@ export class CompareModalComponent implements OnInit, OnDestroy {
           this.arrayManipulationService.getDamageRelationsName(
             damage_relations
           ),
-        baseExperience: pokemon.base_experience,
+        baseExperience: pokemon?.base_experience,
       };
     });
 
     const totals: [number, number] = [
-      this.getTotal(
+      this.utilityService.getTotal(
         pokemonData[0].damageRelations,
         pokemonData[0].baseExperience,
         pokemonData[1].types
       ),
-      this.getTotal(
+      this.utilityService.getTotal(
         pokemonData[1].damageRelations,
         pokemonData[1].baseExperience,
         pokemonData[0].types
--- src/app/services/utilities.service.ts
+++ src/app/services/utilities.service.ts
@@ -11,9 +11,31 @@ export class UtilitiesService {
     };
   }
 
-  getAverage = (a, b) => {
-    return [Math.round((a / (a + b)) * 100), Math.round((b / (a + b)) * 100)];
-  };
+  getAverage = (a, b) => [
+    Math.round((a / (a + b)) * 100),
+    Math.round((b / (a + b)) * 100),
+  ];
 
   hasCommonElement = (arr1, arr2) => arr1?.some((r) => arr2?.includes(r));
+
+  getTotal = (damageRelationsArray, baseExperience, typeArray) => {
+    damageRelationsArray.forEach((el) => {
+      const firstKey = Object.keys(el)?.[0];
+      switch (firstKey) {
+        case 'double_damage_from':
+          if (this.hasCommonElement(el['double_damage_from'], typeArray)) {
+            baseExperience = baseExperience * 0.7;
+          }
+        case 'half_damage_from':
+          if (this.hasCommonElement(el['half_damage_from'], typeArray)) {
+            baseExperience = baseExperience * 1.2;
+          }
+        case 'no_damage_from':
+          if (this.hasCommonElement(el['no_damage_from'], typeArray)) {
+            baseExperience = baseExperience * 1.5;
+          }
+      }
+    });
+    return Math.round(baseExperience * 100) / 100;
+  };
 }
