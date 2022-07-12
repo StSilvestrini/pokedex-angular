e53ce05f49cc573b0c6445106982522f9ad4d21d
StSilvestrini <stefano.silvestrini@alten.it>
2022-07-12 10:47:16 +0200

    chart drawed in pokemon card

--- src/app/components/pokemon-card/pokemon-card.component.ts
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { of, Subscription, switchMap } from 'rxjs';
+import { of, Subscription, switchMap, take } from 'rxjs';
 import type { IPokemonCard } from 'src/app/interfaces';
 import { UtilitiesService } from '../../services/utilities.service';
 import { HttpPokedexService } from '../../services/http.service';
@@ -12,6 +17,8 @@ import {
   transition,
   trigger,
 } from '@angular/animations';
+import Chart from 'chart.js/auto';
+import { getLocaleNumberSymbol } from '@angular/common';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -45,6 +52,7 @@ export class PokemonCardComponent implements OnInit, OnDestroy {
   sendAction: boolean;
   loadDataSubscription: Subscription;
   actionSubscription: Subscription;
+  ctx: any;
 
   constructor(
     private httpService: HttpPokedexService,
@@ -80,6 +88,61 @@ export class PokemonCardComponent implements OnInit, OnDestroy {
           this.sendAction = false;
         }
         this.pokemonCard = pokemonCard;
+        const labels = [];
+        const data = [];
+        pokemonCard?.stats?.forEach((el) => {
+          labels?.push(el?.stat?.name);
+          data?.push(el?.base_stat);
+        });
+        if (!this.ctx) {
+          const ctx: any = document.getElementById('myChart');
+          this.ctx = ctx;
+
+          const chart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+              labels: labels,
+              datasets: [
+                {
+                  data: data,
+                  backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                  ],
+                  borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                  ],
+                  borderWidth: 1,
+                },
+              ],
+            },
+            options: {
+              scales: {
+                y: {
+                  beginAtZero: true,
+                  max: 180,
+                  ticks: {
+                    stepSize: 30,
+                  },
+                },
+              },
+              plugins: {
+                legend: {
+                  display: false,
+                },
+              },
+            },
+          });
+        }
       });
   }
 
