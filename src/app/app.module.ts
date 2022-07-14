62884bdc75b4d9b4a16ee470081635e4804d396b
StSilvestrini <stefano.silvestrini@alten.it>
2022-07-14 11:30:00 +0200

    angular/pwa added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { PokemonListModule } from './components/pokemon-list/pokemon-list.module
 import { SharedModule } from './shared/shared.module';
 import { CompareModalComponent } from './components/compare-modal/compare-modal.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, CompareModalComponent],
@@ -27,6 +29,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     PokemonListModule,
     SharedModule,
     BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
